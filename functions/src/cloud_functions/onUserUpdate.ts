56f433a62b42a7d0cff65898b821f3c0d8086a83
Saikat Karmakar <saikatkarmakar83@gmail.com>
2021-08-17 07:50:35 +0530

    Update onUserUpdate.ts

--- functions/src/cloud_functions/onUserUpdate.ts
+++ functions/src/cloud_functions/onUserUpdate.ts
@@ -2,14 +2,19 @@ import * as functions from "firebase-functions";
 import { auth } from "../firebase";
 
 const onUserUpdate = functions.firestore.document("Users/{userId}").onUpdate((change: any, context: any) => {
-    const preValue = change.before.data();
-    const postValue = change.after.data();
-    if(preValue.isDisabled === false && postValue.isDisabled == true){
-        auth.updateUser(postValue.uid, {
-            disabled: true
-        })
-        .then((result) => console.log(result))
-        .then((error) => console.log(error))
+    try {
+        const preValue = change.before.data();
+        const postValue = change.after.data();
+        if (preValue.isDisabled === false && postValue.isDisabled == true) {
+            auth
+                .updateUser(postValue.uid, {
+                    disabled: true
+                })
+                .then((result) => console.log(result))
+                .catch((error) => console.log(error));
+        }
+    } catch (e) {
+        console.log(e);
     }
 });
 
